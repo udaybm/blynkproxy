42edbacf4ca2fb4655e3dddececc42f9267fc7b6
udaybm <mhatre.uday@gmail.com>
2025-02-11 14:57:22 +0530

    blynk url's for get and update api calls

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());  // Allow requests from any origin
 app.use(express.json());  // Parse JSON request body
 
-// Forward request to Blynk API
+// Forward request to Blynk API,get current pin value
 app.get("/blynk/:token/pin/:pin", async (req, res) => {
     const { token, pin } = req.params;
-    const blynkUrl = `https://blynk.cloud/external/api/get?token=${token}&pin=${pin}`;
-    //const blynkUrl = `https://192.168.1.103:9443/n4uICWW8guhAulM_Rwe2bG8hjcP3TzMB/update/V1?value=1';
+    //const blynkUrl = `https://blynk.cloud/external/api/get?token=${token}&pin=${pin}`;
+    const blynkUrl = `https://192.168.1.103:9443/get?token=${token}&pin=${pin}';
 
     try {
         const response = await axios.get(blynkUrl);
@@ -23,10 +23,11 @@ app.get("/blynk/:token/pin/:pin", async (req, res) => {
     }
 });
 
-// Route to set a pin value
+// Route to set a pin value,Updates a Blynk virtual pin with a new value.
 app.get("/blynk/:token/set/:pin/:value", async (req, res) => {
     const { token, pin, value } = req.params;
-    const blynkUrl = `https://blynk.cloud/external/api/update?token=${token}&${pin}=${value}`;
+    //const blynkUrl = `https://blynk.cloud/external/api/update?token=${token}&${pin}=${value}`;
+    const blynkUrl = `https://192.168.1.103:9443/n4uICWW8guhAulM_Rwe2bG8hjcP3TzMB/update/V1?value=1';
 
     try {
         const response = await axios.get(blynkUrl);
